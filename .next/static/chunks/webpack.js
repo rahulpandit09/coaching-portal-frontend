c811f8b552d004fd48c291b7a793bd8972730602
Rahul kumar <rahul.kashyaprp09@gmail.com>
2026-06-24 22:42:43 +0530

    remove next build files from repo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6134549823fbc2ff"; }
+/******/ 		__webpack_require__.h = function() { return "d3a24b16569012c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
